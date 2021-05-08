fcac6c11753425d236a7f696ac6d60714ab45da7
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-05-08 23:42:00 +0200

    ajouté une barre de navigation

---
+++ lib/bottombar.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+
+class BottomBar extends StatelessWidget {
+  final Function changeIndex;
+  final int indexSelected;
+
+  BottomBar({@required this.changeIndex, @required this.indexSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.school_rounded),
+          label: "Devoirs",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.settings_rounded),
+          label: "Paramètres",
+        ),
+      ],
+      currentIndex: indexSelected,
+      onTap: (int index) => changeIndex(index),
+    );
+  }
+}
