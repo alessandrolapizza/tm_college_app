6552cb8df1c75a09d1f06be455d70d1934bcb216
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-06-28 21:20:13 +0200

    créé carte de devoir

--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:intl/intl.dart";
 
 import 'bottombar.dart';
 
@@ -37,6 +38,73 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           title: Text(_textToShow),
+        ),
+        body: Column(
+          children: [
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 90,
+                width: double.infinity,
+                child: Card(
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(5),
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.orangeAccent, //à construire
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                width: 0.1,
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.calculate, // à construire
+                              color: Colors.white,
+                              size: 45,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 13,
+                              horizontal: 7,
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Mathématiques", // à construire
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.only(top: 3),
+                                  child: Text(
+                                    "Manger + lire l !!",
+                                    overflow: TextOverflow.clip,
+                                  ),
+                                ), // à construire
+                              ],
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.check),
+                          color: Colors.green,
+                        ),
+                      ),
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
         floatingActionButton: _indexSelected == 0
             ? FloatingActionButton(
