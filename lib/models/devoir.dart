62e4241620c79cf5f50e4a28a4f0d7258a322d82
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-06-28 19:37:33 +0200

    modifié la classe "Devoir"

--- lib/models/devoir.dart
+++ lib/models/devoir.dart
@@ -5,11 +5,13 @@ import "matiere.dart";
 class Devoir {
   String titre;
   int id;
+  DateTime dateLimite;
   Matiere matiere;
 
   Devoir({
     @required this.titre,
     @required this.id,
+    @required this.dateLimite,
     this.matiere,
   });
 }
