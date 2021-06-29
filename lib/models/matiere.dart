16258322ae2102eab567303617587a9a695afd9e
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-06-29 15:01:14 +0200

    modifié la classe Matiere

--- lib/models/matiere.dart
+++ lib/models/matiere.dart
@@ -4,10 +4,12 @@ class Matiere {
   final String titre;
   final int id;
   final Color couleurMatiere;
+  final Icons iconMatiere;
 
   Matiere({
     @required this.titre,
     @required this.id,
     @required this.couleurMatiere,
+    @required this.iconMatiere,
   });
 }
