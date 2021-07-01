3da403c7043d46e8807d7f70c50007458ad08ed4
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-07-01 12:15:44 +0200

    créé page pour créer un devoir

---
+++ lib/widgets/page_creer_devoir.dart
@@ -0,0 +1,10 @@
+import "package:flutter/material.dart";
+
+class PageCreerDevoir extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text("Nouveau devoir"),),
+    );
+  }
+}
