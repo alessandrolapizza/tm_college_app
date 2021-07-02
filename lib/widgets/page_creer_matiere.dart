440c6f75cf198813ece3adc7400d8953f57831d7
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-07-03 01:33:04 +0200

    ajouté différents widgets pour créer une matiere

--- lib/widgets/page_creer_matiere.dart
+++ lib/widgets/page_creer_matiere.dart
@@ -1,11 +1,105 @@
 import "package:flutter/material.dart";
 
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+
 class PageCreerMatiere extends StatelessWidget {
+  var controllerTexteMatiere = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nouvelle matière"),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 1.05,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextField(
+                            autofocus: true,
+                            maxLength: 20,
+                            keyboardType: TextInputType.text,
+                            textInputAction: TextInputAction.next,
+                            onEditingComplete: () =>
+                                FocusScope.of(context).nextFocus(),
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.text_fields_rounded),
+                              labelText: "Nom de la matière",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          TextField(
+                            maxLength: 5,
+                            keyboardType: TextInputType.text,
+                            decoration: InputDecoration(
+                              icon:
+                                  Icon(Icons.format_list_numbered_rtl_rounded),
+                              labelText: "Numéro de salle",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Text("Icon "),
+                              Icon(Icons.edit),
+                            ],
+                          ),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Text("Couleur "),
+                              Icon(Icons.edit),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text("Annuler"),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("Enregistrer"),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
