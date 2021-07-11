6f73dbd4e74b7f96a6992f9d082249f4170bcce8
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-07-11 16:10:08 +0200

    créé première apparence pour créer devoir

--- lib/widgets/page_creer_devoir.dart
+++ lib/widgets/page_creer_devoir.dart
@@ -1,10 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 
 class PageCreerDevoir extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Nouveau devoir"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nouveau devoir"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            OutlinedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Matière "),
+                  Icon(Icons.school_rounded),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Text("Date "),
+                      Icon(Icons.date_range_rounded),
+                    ],
+                  ),
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Text("Importance "),
+                      Icon(Icons.format_list_numbered_rtl_rounded),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 3,
+              decoration: InputDecoration(
+                labelText: "Contenu",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
