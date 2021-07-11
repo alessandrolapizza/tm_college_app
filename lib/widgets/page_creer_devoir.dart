93acb2f25723611315fa785543c38bbbfc333428
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-07-11 17:30:55 +0200

    modifié apparence

--- lib/widgets/page_creer_devoir.dart
+++ lib/widgets/page_creer_devoir.dart
@@ -9,49 +9,89 @@ class PageCreerDevoir extends StatelessWidget {
         title: Text("Nouveau devoir"),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            OutlinedButton(
-              onPressed: () {},
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Matière "),
-                  Icon(Icons.school_rounded),
-                ],
-              ),
-            ),
-            Row(
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width / 1.05,
+            child: Column(
               children: [
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Text("Date "),
-                      Icon(Icons.date_range_rounded),
-                    ],
-                  ),
+                Wrap(
+                  runSpacing: 15,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Matière "),
+                          Icon(Icons.school_rounded),
+                        ],
+                      ),
+                    ),
+                    TextField(
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        labelText: "Contenu",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 2.5,
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Date "),
+                                Icon(Icons.date_range_rounded),
+                              ],
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width / 2.5,
+                          child: OutlinedButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Importance "),
+                                Icon(Icons.format_list_numbered_rtl_rounded),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Text("Importance "),
-                      Icon(Icons.format_list_numbered_rtl_rounded),
-                    ],
+                Expanded(
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Text("Annuler"),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5),
+                        ),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Text("Enregistrer"),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
-            TextField(
-              keyboardType: TextInputType.multiline,
-              maxLines: 3,
-              decoration: InputDecoration(
-                labelText: "Contenu",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
