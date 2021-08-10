c02af2fc8462f0fed5aa0b3ef2eac1eb47f97a0f
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-08-10 18:04:27 +0200

    séparé App du fichier main.dart

---
+++ lib/widgets/app.dart
@@ -0,0 +1,65 @@
+import "package:flutter/material.dart";
+import "package:flutter_localizations/flutter_localizations.dart";
+import "package:flutter/services.dart";
+import 'package:tm_college_app/widgets/create_homework_page.dart';
+import 'package:tm_college_app/widgets/create_subject_page.dart';
+import 'package:tm_college_app/widgets/view_homework_page.dart';
+import "./home_page.dart";
+import "../models/base_de_donnees.dart";
+
+import "./page_visualiser_matiere.dart";
+
+class App extends StatelessWidget {
+  final BaseDeDonnees database;
+
+  App({@required this.database});
+
+  static MaterialColor toMaterialColor(int colorValue) {
+    Map<int, Color> colorMap = {
+      50: Color(colorValue),
+      100: Color(colorValue),
+      200: Color(colorValue),
+      300: Color(colorValue),
+      400: Color(colorValue),
+      500: Color(colorValue),
+      600: Color(colorValue),
+      700: Color(colorValue),
+      800: Color(colorValue),
+      900: Color(colorValue),
+    };
+    print(Color(colorValue));
+    return MaterialColor(colorValue, colorMap);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+      [
+        DeviceOrientation.portraitUp,
+      ],
+    );
+    return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale(
+          "fr",
+          "FR",
+        ),
+      ],
+      title: "TM_COLLEGE_APP", //Provisoire
+      initialRoute: "/",
+      routes: {
+        "/": (_) => HomePage(database),
+        "/create_subject_page": (_) => CreateSubjectPage(database),
+        "/create_homework_page": (_) => CreateHomeworkPage(database),
+        "/page_visualiser_matiere": (_) => PageVisualiserMatiere(),
+        "/homework_details_page": (_) => ViewHomeworkPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: toMaterialColor(4283522728),
+      ),
+    );
+  }
+}
--- lib/widgets/main.dart
+++ lib/widgets/main.dart
@@ -1,75 +1,18 @@
 import 'package:flutter/material.dart';
-import "package:flutter/services.dart";
 import "package:intl/intl.dart";
-import "package:flutter_localizations/flutter_localizations.dart";
-import 'package:tm_college_app/widgets/create_homework_page.dart';
-import 'package:tm_college_app/widgets/create_subject_page.dart';
-import 'package:tm_college_app/widgets/view_homework_page.dart';
-import "./home_page.dart";
-
-import "./page_visualiser_matiere.dart";
+import "./app.dart";
 import '../models/base_de_donnees.dart';
 
-BaseDeDonnees bD;
-
-const Map<int, Color> color = {
-  50: Color(4283522728),
-  100: Color(4283522728),
-  200: Color(4283522728),
-  300: Color(4283522728),
-  400: Color(4283522728),
-  500: Color(4283522728),
-  600: Color(4283522728),
-  700: Color(4283522728),
-  800: Color(4283522728),
-  900: Color(4283522728),
-};
-
-MaterialColor customMaterialColor = MaterialColor(4283522728, color);
+BaseDeDonnees database;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  bD = BaseDeDonnees();
+  database = BaseDeDonnees();
 
-  await bD.defCheminMatieres();
+  await database.defCheminMatieres();
 
   Intl.defaultLocale = "fr";
 
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  
-  @override
-  Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations(
-      [
-        DeviceOrientation.portraitUp,
-      ],
-    );
-    return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: [
-        Locale(
-          "fr",
-          "FR",
-        ),
-      ],
-      title: "TM_COLLEGE_APP", //Provisoire
-      initialRoute: "/",
-      routes: {
-        "/": (_) => HomePage(bD),
-        "/create_subject_page": (_) => CreateSubjectPage(bD),
-        "/create_homework_page": (_) => CreateHomeworkPage(bD),
-        "/page_visualiser_matiere": (_) => PageVisualiserMatiere(),
-        "/homework_details_page": (_) => ViewHomeworkPage(),
-      },
-      theme: ThemeData(
-        primarySwatch: customMaterialColor,
-      ),
-    );
-  }
+  runApp(App(database: database));
 }
