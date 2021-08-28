eda86d082bd3efc837e01896773a7f6e15f222a8
Alessandro De Zen <dzn.alessandro@gmail.com>
2021-08-29 00:23:26 +0200

    modifié pages de configurations

---
+++ lib/widgets/date_picker_outlined_button.dart
@@ -0,0 +1,41 @@
+import "package:flutter/material.dart";
+
+class DatePickerOutlinedButton extends StatelessWidget {
+  final bool enabled;
+
+  final String text;
+
+  final Function function;
+
+  DatePickerOutlinedButton({
+    @required this.enabled,
+    @required this.text,
+    @required this.function,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: OutlinedButton(
+        style: ButtonStyle(
+          side: !enabled
+              ? MaterialStateProperty.all(
+                  BorderSide(color: Colors.grey),
+                )
+              : null,
+          foregroundColor:
+              !enabled ? MaterialStateProperty.all(Colors.grey) : null,
+        ),
+        onPressed: () => enabled ? function() : null,
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(text + " "),
+            Icon(Icons.date_range),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/one_time_introduction_screen.dart
+++ lib/widgets/one_time_introduction_screen.dart
@@ -2,9 +2,10 @@ import "package:flutter/material.dart";
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tm_college_app/models/notifications.dart';
+import 'package:tm_college_app/widgets/one_time_introduction_date_configuration_body.dart';
 import 'package:tm_college_app/widgets/one_time_introduction_notifications_setting_tile.dart';
 
-class OneTimeIntroductionScreen extends StatelessWidget {
+class OneTimeIntroductionScreen extends StatefulWidget {
   final Notifications notifications;
 
   final SharedPreferences sharedPreferences;
@@ -15,21 +16,31 @@ class OneTimeIntroductionScreen extends StatelessWidget {
   });
 
   @override
+  _OneTimeIntroductionScreenState createState() =>
+      _OneTimeIntroductionScreenState();
+}
+
+class _OneTimeIntroductionScreenState extends State<OneTimeIntroductionScreen> {
+  bool _show = false;
+
+  void _showDoneButton({@required bool show}) {
+    setState(() => _show = show);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return IntroductionScreen(
+      showDoneButton: _show,
       color: Colors.black,
       done: Text(
         "Done",
         style: TextStyle(fontWeight: FontWeight.w600),
       ),
       onDone: () async {
-        await sharedPreferences.setBool("introductionSeen", true);
+        await widget.sharedPreferences.setBool("introductionSeen", true);
         Navigator.pushReplacementNamed(context, "/");
       },
-      showNextButton: true,
       next: Icon(Icons.arrow_forward),
-      showSkipButton: true,
-      skip: Text("Passer"),
       dotsDecorator: DotsDecorator(
         color: Colors.grey,
         activeColor: Theme.of(context).primaryColor,
@@ -43,34 +54,67 @@ class OneTimeIntroductionScreen extends StatelessWidget {
               'assets/images/transparent_icon.png',
             ),
           ),
-          body: "Une application simple et efficace pour gérer tes devoirs.",
+          decoration: PageDecoration(
+            bodyFlex: 1,
+            imageFlex: 2,
+          ),
+          useScrollView: true,
+          body:
+              "Une application simple et efficace pour gérer tes notes et devoirs.",
+        ),
+        PageViewModel(
+          title: "Avant de continuer",
+          image: Icon(
+            Icons.emoji_people_rounded,
+            size: 200,
+            color: Colors.orangeAccent,
+          ),
+          body:
+              "Cette application a été conçue dans la cadre d'un travail de maturité à Genève. Elle est donc pour l'instant restreinte au fonctionnement général des écoles qui font partie du secondaire 2. (Collège, ECG)",
+          useScrollView: true,
         ),
         PageViewModel(
           title: "Activer les notifications ?",
           image: Icon(
-            Icons.notifications,
-            size: 100,
+            Icons.edit_notifications_rounded,
+            size: 200,
+            color: Theme.of(context).primaryColor,
           ),
-          bodyWidget: Column(
-            children: [
-              Text(
-                "Les notifications te permettent de te rappeler de tes devoirs aux heures qui te conviennent. Elles sont désactivables et customisables à tout moment dans les paramètres de l'application.",
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.normal,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 10),
-              ),
-              OneTimeIntroductionNotificationsSettingTile(
-                sharedPreferences: sharedPreferences,
-                notifications: notifications,
-              )
-            ],
+          body:
+              "Les notifications te permettent de te rappeler de tes devoirs aux heures qui te conviennent. Elles sont désactivables et customisables à tout moment dans les paramètres de l'application.",
+          decoration: PageDecoration(
+            bodyFlex: 10,
+            imageFlex: 9,
           ),
-          useScrollView: false,
+          useScrollView: true,
+          footer: OneTimeIntroductionNotificationsSettingTile(
+            sharedPreferences: widget.sharedPreferences,
+            notifications: widget.notifications,
+          ),
+        ),
+        PageViewModel(
+          title: "Un peu de configuration",
+          image: Icon(
+            Icons.edit,
+            size: 200,
+            color: Theme.of(context).primaryColor,
+          ),
+          body:
+              "Attention ! Les dates fournies ne sont plus modifiables après validation. Pour les changers il faudra \"Commencer une nouvelle Année Scolaire\" dans les paramètres.",
+          useScrollView: true,
+        ),
+        PageViewModel(
+          title: "Dates",
+          image: Icon(
+            Icons.date_range,
+            size: 200,
+            color: Theme.of(context).primaryColor,
+          ),
+          bodyWidget: OneTimeIntroductionDateConfigurationBody(
+            sharedPreferences: widget.sharedPreferences,
+            confirmDateConfigurationFunction: _showDoneButton,
+          ),
+          useScrollView: true,
         ),
       ],
     );
